6268de78211605a7b004c6fc3553d8373febbaa7
Vikash Vishwakarma <vikashv@infinite.com>
2024-03-06 10:47:53 +0530

    Test

---
+++ C_sharp/Assesments/Test_2/Test_2/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Product
+{
+    public int Product_Id { get; set; }
+    public string Product_Name { get; set; }
+    public double Product_Price { get; set; }
+}
+
+class ProductSorter
+{
+    public static void Main()
+    {
+        Product[] products = new Product[10];
+
+        Console.WriteLine("-----Getting Product Details-----");
+
+        Console.WriteLine("Enter details of products -> ");
+        for (int i = 0; i < products.Length; i++)
+        {
+            Console.Write($"Product {i + 1}  ID -> ");
+            int productId = int.Parse(Console.ReadLine());
+            Console.Write($"Product {i + 1}  Name: ");
+            string productName = Console.ReadLine();
+            Console.Write($"Product {i + 1}  Price: ");
+            double price = double.Parse(Console.ReadLine());
+
+            products[i] = new Product { Product_Id = productId, Product_Name = productName, Product_Price = price };
+        }
+
+        // Sorting 
+        Array.Sort(products, (a, b) => a.Product_Price.CompareTo(b.Product_Price));
+
+     
+        Console.WriteLine("Sorted Products based on Price:");
+        foreach (Product product in products)
+        {
+            Console.WriteLine($"Product ID: {product.Product_Id}, Name: {product.Product_Name}, Price: {product.Product_Price}");
+        }
+    }
+}
---
+++ C_sharp/Assesments/Test_2/Test_2/Student_Test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+abstract class Student
+{
+    public string Student_Name { get; set; }
+    public int Student_Id { get; set; }
+    public double Grade { get; set; }
+
+    // Abstract method 
+    public abstract bool IsPassed(double grade);
+}
+
+class Undergraduate : Student
+{
+    //Parametrized constructor
+    public Undergraduate(string Student_Name, int Student_Id, double Grade)
+    {
+        this.Student_Name = Student_Name;
+        this.Student_Id = Student_Id;
+        this.Grade = Grade;
+    }
+
+    public override bool IsPassed(double Grade)
+    {
+        return Grade > 70.0;
+    }
+}
+
+class Graduate : Student
+{
+  
+    public Graduate(string Student_Name, int Student_Id, double Grade)
+    {
+        this.Student_Name = Student_Name;
+        this.Student_Id = Student_Id;
+        this.Grade = Grade;
+    }
+
+
+    public override bool IsPassed(double Grade)
+    {
+        return Grade > 80.0;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Undergraduate class
+        Undergraduate undergraduate = new Undergraduate("Vikash Vishwkarma", 101, 71 );
+        Console.WriteLine($"{undergraduate.Student_Name} Passed: {undergraduate.IsPassed(undergraduate.Grade)}");
+
+        // Graduate class
+        Graduate graduate = new Graduate("Vikash vishwakarma", 102, 87);
+        Console.WriteLine($"{graduate.Student_Name} Passed: {graduate.IsPassed(graduate.Grade)}");
+    }
+}
