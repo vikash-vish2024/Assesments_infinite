3840f147c221ec57cc69b2f1da5d548460afd7a4
vikash-vish2024 <160992850+vikash-vish2024@users.noreply.github.com>
2024-03-17 00:42:46 +0530

    Assign_1 & Assign_2

---
+++ SQL/Assignment/Assignment_1/Assignment_1.sql
@@ -0,0 +1,109 @@
+Create database Assignment
+use Assignment
+
+--Department Table
+create table Department
+
+(Deptno numeric(2) primary key,
+Dname varchar(15) not null, Loc varchar(20))
+
+--Client Table
+create table Clients
+
+(Client_id numeric(4) primary key,
+Cname varchar(30) not null,
+Address varchar(40),Email varchar(30) unique,
+phone numeric(10),Business varchar(20) ) 
+
+--Employee Table
+create table Employee
+(Empno numeric(4) primary key,
+Ename varchar(20) not null,
+job varchar(15),
+salary numeric(7),
+Deptno numeric(2) foreign key references department(Deptno))
+
+--Project Table
+create table Project
+(Project_ID numeric(3) primary key,
+Descr varchar(30) not null,
+Start_date date,
+Planned_End_Date date,
+Actual_End_Date date, 
+Budget numeric(10),
+Client_ID numeric(4) foreign key references Clients(client_Id))
+
+--Adding constrain to date
+alter table Project 
+add constraint CK_Actual_End_Date
+check(Actual_End_Date>Planned_End_Date)
+
+--Employee Project Task Table
+create table Emp_Project_Task(
+Project_ID numeric(3),
+Empno numeric(4),
+Start_Date date,
+End_Date date,
+Task varchar(25),
+Status varchar(15),
+primary key(Project_ID,Empno),
+foreign key (Project_ID)references Project(Project_ID),
+foreign key (Empno)references Employee(Empno))
+
+
+
+--(-------------------------------------------------------)--
+--inserting client data
+insert into clients values
+(1001,'ACME utilities','Noida','Contact@acmeutil.com','9567880032','Manufacturing'),
+(1002,'Trackon Consultant','Mumbai','Consult@trackon.com','8734210090','Consultant'),
+(1003,'MoneySaver Distributors','Kolkata','save@moneysaver.com','7799886655','Reseller'),
+(1004,'Lawful Crop','Chennai','justice@lawful.com','9210342219','Professional')
+
+select * from clients
+
+--inserting Department information
+insert into Department values
+(10,'Design','Pune'),
+(20,'Development','pune'),
+(30,'Testing','Mumbai'),
+(40,'Document','Mumbai')
+
+--inserting Employees data
+insert into Employee values
+(7001,'Sandeep','Analyst',25000,10),
+(7002,'Rajesh','Designer',30000,10),
+(7003,'Madhav','Developer',40000,20),
+(7004,'Manoj','Developer',40000,20),
+(7005,'Abhay','Designer',35000,10),
+(7006,'Uma','Tester',30000,30),
+(7007,'Gita','Tech. Writer',30000,40),
+(7008,'Priya','Tester',35000,30),
+(7009,'Nutan','Developer',45000,20),
+(7010,'Smita','Analyst',20000,10),
+(7011,'Anand','Project Mgr',65000,10)
+
+
+--Project Information
+insert into Project values
+(401,'Inventory','2011-04-01','2011-10-01','2011-10-31',150000,1001),
+(402,'Accounting','2011-08-01','2012-01-01',null,150000,1002),
+(403,'Payroll','2011-12-11','2011-12-31',null,150000,1003),
+(404,'Contact mgmt','2011-11-01','2011-12-31',null,150000,1004)
+
+select * from project
+
+--insert EmpProject task 
+insert into Emp_Project_Task values
+(401,7001,'2011-04-01','2011-04-20','System Analysis','Completed'),
+(401,7002,'2011-04-21','2011-05-30','System Design','Completed'),
+(401,7003,'2011-06-01','2011-07-15','Coding','Completed'),
+(401,7004,'2011-07-18','2011-09-01','Coding','Completed'),
+(401,7006,'2011-09-03','2011-09-15','Testing','Completed'),
+(401,7009,'2011-09-18','2011-10-05','Code Change','Completed'),
+(401,7008,'2011-10-06','2011-10-16','Testing','Completed'),
+(401,7007,'2011-10-06','2011-10-22','Documentation','Completed'),
+(402,7011,'2011-10-22','2011-10-31','Sign Off','Completed'),
+(402,7010,'2011-08-01','2011-08-20','System Analysis','Completed'),
+(402,7002,'2011-08-22','2011-09-30','System Design','Completed'),
+(402,7004,'2011-10-01',null,'Coding','in Process')
---
+++ SQL/Assignment/Assignment_2/Assignment_2.sql
@@ -0,0 +1,130 @@
+create database Assignment_2
+use Assignment_2
+
+--Two tables to be created in the database that may be used for the following exercises. 
+--Their structure is as follows:
+
+create table Employees
+(Empno numeric(4)unique,
+Ename varchar(20),
+Job varchar(20),
+Mgr_id numeric(4),
+hiredate date,
+salary numeric(6),
+comm numeric(4),
+deptno numeric(2) foreign key references Department(Deptno) )
+
+create table Department
+(Deptno numeric (2) primary key,
+Dname varchar(20),
+Location varchar(15))
+
+Select * from Department
+Select * from Employees
+
+--insert data into Department
+insert into Department values
+(10,'ACCOUNTING','NEW YORK'),
+(20,'RESEARCH','DALLAS'),
+(30,'SALES','CHICAGO'),
+(40,'OPERATION','BOSTON')
+
+--Inserting data into Employees
+
+Insert into Employees  values
+(7369,'SMITH', 'CLERK',7902,'1980-12-17',800,NULL,20),
+(7499,'ALLEN','SALESMAN',7698,'1981-02-20',1600,300,30),
+(7521,'WARD','SALESMAN',7698,'1981-02-22',1250,500,30),
+(7566,'JONES','MANAGER',7839,'1981-04-02',2975,NULL,20),
+(7654,'MARTIN','SALESMAN', 7698,'1981-09-28',1250,1400,30),
+(7698,'BLAKE','MANAGER', 7839,'1981-05-01',2850,NULL,30),
+(7782,'CLARK','MANAGER', 7839,'1981-06-09',2450,NULL,10),
+(7788,'SCOTT','ANALYST', 7566,'1987-04-19',3000,NULL,20),
+(7839,'KING','PRESIDENT',NULL,'1981-NOV-17',5000,NULL,10),
+(7844,'TURNER','SALESMAN',7698,'1981-09-08',1500,0,30),
+(7876,'ADAMS','CLERK',7788, '1987-04-23',1100,NULL,20),
+(7900,'JAMES','CLERK',7698, '1981-12-03',950,NULL,30),
+(7902,'FORD','ANALYST',7566,'1981-12-03',3000,NULL,20),
+(7934,'MILLER','CLERK',7782,'1982-01-23',1300,NULL, 10)
+
+--Write and execute the necessary SQL statements to perform the following tasks.
+
+--List all employees whose name begins with 'A'
+select * from Employees where Ename like '[A]%'
+
+--Select all those employees who don't have a manager
+select * from Employees where Mgr_id is null
+
+-- List employee name, number and salary for those employees who earn in the range 1200 to 1400.
+select * from Employees where salary between 1200 and 1400
+
+--Give all the employees in the RESEARCH department a 10% pay rise.
+--Verify that this has been done by listing all their details before and after the rise
+
+--Before Rise
+select Empno, Deptno, Salary as OriginalSalary
+from Employees
+		where Deptno = (select Deptno from Department where Dname = 'RESEARCH');
+
+--After Rise
+select  Empno, Deptno, (Salary+(salary*10)/100) as 'Salary After Rise'
+from Employees
+		where Deptno = (select Deptno from Department where Dname = 'RESEARCH');
+
+
+--Find the number of CLERKS employed. Give it a descriptive heading.
+select COUNT(*) AS NumberOfClerks
+from Employees
+		where Job = 'CLERK'
+
+--Find the average salary for each job type and the number of people employed in each job.
+select Job, AVG(Salary) AS 'Average Salary', count(*) as 'Number of empoyee' 
+from Employees
+group by Job
+
+--List the employees with the lowest and highest salary.
+select * from Employees 
+	where salary =(select max(salary) from Employees)
+	  or salary =(select min(salary) from Employees)
+	
+--List full details of departments that don't have any employees.
+select dept.Deptno,dept.Dname,dept.Location
+from Department dept
+left join Employees em on dept.Deptno=em.deptno
+where em.Empno is null
+
+--Get the names and salaries of all the analysts earning more than 1200 who are based in department 20.
+--Sort the answer by ascending order of name.
+select * from Employees
+where(job='Analyst' and salary>1200 and deptno=20)
+
+--For each department, list its name and number together with the total salary paid to employees in that department.
+select dept.Dname as 'Department Name',dept.Deptno as 'Departmet No',sum(Emp.salary) as 'Total Salary'
+from Department dept left join
+Employees emp on dept.Deptno=emp.deptno
+group by dept.Deptno,dept.Dname
+order by dept.Deptno
+
+--Find out salary of both MILLER and SMITH.
+select Ename,salary from Employees
+where (Ename='miller' or Ename='smith')
+order by Ename
+
+-- Find out the names of the employees whose name begin with ‘A’ or ‘M’.
+select Ename from Employees
+where Ename like 'A%M'
+
+--Compute yearly salary of SMITH
+select ename,(salary*12) as 'yearly salary' from Employees
+where Ename=('smith')
+
+--List the name and salary for all employees whose salary is not in the range of 1500 and 2850. 
+select Ename,salary from Employees
+where salary not between 1500 and 2850
+order by salary
+
+--Find all managers who have more than 2 employees reporting to them
+select  Mgr_id, count(Empno) as EMP_COUNT
+from Employees
+group by Mgr_id
+having count(EMPNO) > 2;
